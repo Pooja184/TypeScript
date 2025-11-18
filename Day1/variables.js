6dceea67ffeb7f2801658b17b774917d250221fd
pxxja <poojabhambid2004@gmail.com>
2025-11-18 19:25:36 +0530

    lecture 1

---
+++ Day1/variables.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello Pooja";
+console.log(greetings);
+var hero;
+function getHero() {
+    return "thor";
+}
+hero = getHero();
+function addTwo(num) {
+    // return num+4;
+    return "hello";
+}
+addTwo(2);
+var hello = addTwo(5);
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+loginUser("pooja", "p@gmail.com");
---
+++ Day1/variables.ts
@@ -0,0 +1,28 @@
+let greetings:string ="Hello Pooja";
+console.log(greetings);
+
+
+
+
+let hero: string;
+function getHero(){
+    return "thor";
+}
+hero=getHero();
+
+
+
+function addTwo(num:number){
+    return num+4;
+    // return "hello"
+}
+addTwo(2);
+// let hello=addTwo(5)
+
+
+
+let loginUser=(name:string,email:string,isPaid:boolean=false)=>{
+
+}
+loginUser("pooja","p@gmail.com")
+export {}
