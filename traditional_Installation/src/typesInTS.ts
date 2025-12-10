ea2b26c2f390b4a10f39e9cff9da6fac442489a6
pxxja <poojabhambid2004@gmail.com>
2025-12-10 16:30:56 +0530

    learn abt type inference and type annotation

---
+++ traditional_Installation/src/typesInTS.ts
@@ -0,0 +1,12 @@
+// ----------------type inference----------------------
+// TypeScript automatically guesses the type of a variable.
+// You don’t need to write the type.
+let age = 20;      // TypeScript infers type as number
+let name = "Pooja"; // inferred as string
+
+
+
+// ------------------------- Type annotation---------------------
+// Type annotation means we explicitly define the data type.
+let age1: number = 20;
+let name1: string = "Pooja";
