5d965b2766c9fb4380a6725739b0d0d6ae4fab08
pxxja <poojabhambid2004@gmail.com>
2025-12-10 16:21:59 +0530

    install typescript in traditional way

---
+++ traditional_Installation/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `hello ${person}`;
+}
+const name = "Pooja";
+console.log(greet(name));
+//# sourceMappingURL=index.js.map
