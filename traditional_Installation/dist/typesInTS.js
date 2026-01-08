2692d52d407d8c2cfc638744d3ae5c12614d18ed
pxxja <poojabhambid2004@gmail.com>
2026-01-08 15:48:56 +0530

    types

---
+++ traditional_Installation/dist/typesInTS.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// ----------------type inference----------------------
+// TypeScript automatically guesses the type of a variable.
+// You don’t need to write the type.
+let age = 20; // TypeScript infers type as number
+let name = "Pooja"; // inferred as string
+// ------------------------- Type annotation---------------------
+// Type annotation means we explicitly define the data type.
+let age1 = 20;
+let name1 = "Pooja";
+//# sourceMappingURL=typesInTS.js.map
